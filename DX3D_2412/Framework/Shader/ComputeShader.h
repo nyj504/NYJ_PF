4685235e70078a19d03661e1d97f4aa3c72574f9
nyj504 <dydwn980511@gmail.com>
2025-03-20 22:03:27 +0900

    .
    .

--- DX3D_2412/Framework/Shader/ComputeShader.h
+++ DX3D_2412/Framework/Shader/ComputeShader.h
@@ -1,16 +1,1 @@
 #pragma once
-
-class ComputeShader : public Shader
-{
-private:
-    friend class Shader;
-
-    ComputeShader(wstring file);
-    ~ComputeShader();
-
-public:
-    virtual void Set() override;
-
-private:
-    ID3D11ComputeShader* shader;
-};
